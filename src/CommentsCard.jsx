323801950a8e4ab714b89ff128786dd377ccd64d
Kasha Malloy <kashamalloy@gmail.com>
2023-12-14 12:42:30 +0000

    adding ability to delete comment

--- src/CommentsCard.jsx
+++ src/CommentsCard.jsx
@@ -1,3 +1,6 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
 
 const CommentsCard = (props) => {
     const { comment } = props;
@@ -7,6 +10,10 @@ const CommentsCard = (props) => {
             <li><p id="author">{comment.author} on {comment.created_at.slice(0, 10)}</p></li>
             <li><p>{comment.votes} comments  </p></li>
             <li><p>{comment.votes} votes</p></li>
+            <IconButton aria-label="delete" color="primary" id="delete-button">
+                <DeleteIcon />
+                Delete comment
+            </IconButton>
         </div>
     )
 }
