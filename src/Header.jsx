e22013896116aa3e59e132ffebe53497ddbb8d75
Kasha Malloy <kashamalloy@gmail.com>
2024-01-18 21:24:55 +0000

    added styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,6 +6,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import news from './assets/news.png';
+
 
 export default function Header() {
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -14,6 +16,7 @@ export default function Header() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" id="appbar">
                 <Toolbar>
+                    <img src={news} alt="news logo" />
                     <Typography id="header" variant="h2" component="div" sx={{ flexGrow: 1 }}>
                         NC News
                     </Typography>
